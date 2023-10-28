8473352953bd0dc2a07201340f3b0433986055fb
Augusto Giles <augustogiles@gmail.com>
2023-10-28 15:15:14 +0100

    add openzeppelin files

---
+++ @openzeppelin/contracts/utils/Context.sol
@@ -0,0 +1,38 @@
+/*
+  
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+
+Find any smart contract, and build your project faster: https://www.cookbook.dev
+Twitter: https://twitter.com/cookbook_dev
+Discord: https://discord.gg/WzsfPcfHrk
+
+Find this contract on Cookbook: https://www.cookbook.dev/contracts/0x079a889eb69013d451ecf45377258948116e2b3e-MarketNG/?utm=code
+*/
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+/**
+ * @dev Provides information about the current execution context, including the
+ * sender of the transaction and its data. While these are generally available
+ * via msg.sender and msg.data, they should not be accessed in such a direct
+ * manner, since when dealing with meta-transactions the account sending and
+ * paying for execution may not be the actual sender (as far as an application
+ * is concerned).
+ *
+ * This contract is only required for intermediate, library-like contracts.
+ */
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
