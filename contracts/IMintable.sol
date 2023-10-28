c408363a81807a8afbaced0367936159070164ab
Augusto Giles <augustogiles@gmail.com>
2023-10-28 15:13:22 +0100

    Add files via upload

---
+++ contracts/IMintable.sol
@@ -0,0 +1,23 @@
+/*
+  
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+
+Find any smart contract, and build your project faster: https://www.cookbook.dev
+Twitter: https://twitter.com/cookbook_dev
+Discord: https://discord.gg/WzsfPcfHrk
+
+Find this contract on Cookbook: https://www.cookbook.dev/contracts/0x079a889eb69013d451ecf45377258948116e2b3e-MarketNG/?utm=code
+*/
+
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+interface IMintable {
+    function mint(address to, bytes memory data) external;
+}
---
+++ contracts/IWETH.sol
@@ -0,0 +1,27 @@
+/*
+  
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██    ██████  ███████ ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██      ██    ██
+██      ██    ██ ██    ██ █████   ██████  ██    ██ ██    ██ █████      ██   ██ █████   ██    ██
+██      ██    ██ ██    ██ ██  ██  ██   ██ ██    ██ ██    ██ ██  ██     ██   ██ ██       ██  ██
+ ██████  ██████   ██████  ██   ██ ██████   ██████   ██████  ██   ██ ██ ██████  ███████   ████
+
+Find any smart contract, and build your project faster: https://www.cookbook.dev
+Twitter: https://twitter.com/cookbook_dev
+Discord: https://discord.gg/WzsfPcfHrk
+
+Find this contract on Cookbook: https://www.cookbook.dev/contracts/0x079a889eb69013d451ecf45377258948116e2b3e-MarketNG/?utm=code
+*/
+
+// SPDX-License-Identifier: Unlicensed
+
+pragma solidity ^0.8.0;
+pragma abicoder v2;
+
+import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+
+    function withdraw(uint256 wad) external;
+}
